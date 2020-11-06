578819623f8fbf85e4607f2af79f82232da54c25
jerry <brownjer3@gmail.com>
2020-11-06 12:41:06 -0800

    commit tree 42cb83d6f55b91f3e7d3a3c58cd74fc8beb43679

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+  end
+
+  def kind
+    #returns, as a symbol, its type
+    if @side1+@side2>@side3 && @side2+@side3>@side1 && @side3+@side1>@side2
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif @side1==@side2 || @side2==@side3 || @side1==@side3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  # Custom Errors section
+  class TriangleError < StandardError
+    def message
+      "That's not a valid triangle!!"
+    end
+  end
+
 end
